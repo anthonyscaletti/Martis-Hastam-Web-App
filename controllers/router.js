0833934b108710b812f29c98a57d60b15edf571f
WTony12 <wagdianthony@gmail.com>
2017-06-12 23:08:45 -0300

    Added file upload capability

--- controllers/router.js
+++ controllers/router.js
@@ -1,8 +1,10 @@
 const bodyParser = require('../middleware/bodyParser');
+const upload = require('express-fileupload');
 
 module.exports = function(app){
+    //Middleware express-fileupload
+    app.use(upload());
     //Middleware body parser
-    //const urlEncodedParser = bodyParser.urlencoded({ extended: false });
     const urlEncodedParser = bodyParser(app);
     //Route to Homepage
     app.get("/", function(req, res){
@@ -11,6 +13,23 @@ module.exports = function(app){
     //Route to Homepage
     app.get("/home", function(req, res){
         res.render("index");
+    });
+    //route to Submit page
+    app.post("/submit", function(req, res){
+        if(req.files)
+        {
+            var ptext = req.files.f1;
+
+            ptext.mv("./DATA/" + ptext.name, function(err){
+                if(err)
+                {
+                    console.log(err);
+                    res.send("ERROR OCCURED");
+                }
+                else
+                    res.send("FILE RECEIVED");
+            });
+        }
     });
     // Handle 404
     app.use(function(req, res, next){
