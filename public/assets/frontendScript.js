7ee52b504b451da1601ee21679a95fcd021a8863
WTony12 <wagdianthony@gmail.com>
2017-06-15 13:59:31 -0300

    Jquery

--- public/assets/frontendScript.js
+++ public/assets/frontendScript.js
@@ -1,16 +1,5 @@
-// $(document).ready(function(){
-//     $("#form").on("submit", function(){
-//         var user = $("#inp").val();
-//         var pass = $("#pass").val();
-//
-//         $.ajax({
-//             url: "/submit",
-//             type: "POST",
-//             dataType: "JSON",
-//             data: {u: user, p: pass},
-//             success: function(data){
-//                 alert(data);
-//             }
-//         });
-//     });
-// });
+$(document).ready(function(){
+    $("#form").on("submit", function(){
+        $("#initialInp").hide();
+    });
+});
