4bf57055c3b1baab0eae18d5d9769e8222c07a1c
WTony12 <wagdianthony@gmail.com>
2017-06-21 19:04:26 -0300

    Reenabled security proocols

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const app = express();
 app.use(cookieParser());
 app.use(session({secret: "Shh, its a secret!", resave: false, saveUninitialized: false}));
 //Enforce HTTPS connections
-//enforceSSL(app);
+enforceSSL(app);
 //Employ helmet module's security suite
-//helmet(app);
+helmet(app);
 //Use EJS view engine
 app.set("view engine", "ejs");
 //Specify where the static files are located
